fab874240cd5286a606ec1ab284a83fccdd36c5e
alba-garcia09 <albagarlaz@gmail.com>
2024-09-09 16:58:38 +0200

    main work inprogram page

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { useRoutes } from 'react-router-dom';
 import Home from '../src/pages/Home/Home.jsx'
 import Tickets from '../src/pages/Tickets/Tickets';
+import Program from '../src/pages/Program/Program.jsx';
 
 
 function AppRouter() {
@@ -13,6 +14,10 @@ function AppRouter() {
       {
         element: <Tickets />,
         path: '/tickets',
+      },
+      {
+        element: <Program />,
+        path: '/program',
       },
     ],
   )
